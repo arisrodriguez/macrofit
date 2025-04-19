7af4b6b58b610a056bc9c1633aac9585b87acb95
Aristides Rodríguez <12479256+arisrodriguez@users.noreply.github.com>
2025-04-19 14:50:21 -0500

    ajuste del texto del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@
 export const Footer = () => {
     return (
         <footer className="flex items-center justify-center gap-2 text-center text-lg py-8 md:pt-5">
-            <p className="font-light text-gray-600 dark:text-neutral-300">Coded by</p>
+            <p className="font-light text-gray-600 dark:text-neutral-300">Built and designed by </p>
             <p className="font-bold dark:font-normal text-violet-700 md:dark:text-white relative group">
                 <a target="_blank" rel="noopener noreferrer" href="https://github.com/arisrodriguez" className="relative group">
-                    Built and designed by Aris Rodríguez.
+                    Aris Rodríguez.
                     <span className="absolute left-0 bottom-[-2px] h-[2px] w-0 bg-violet-700 transition-all duration-300 group-hover:w-full group-hover:bg-violet-700"></span>
                 </a>
             </p>
